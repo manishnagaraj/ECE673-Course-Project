a26c84611ec6e7ef1511a4ec7f6bfa366b6ac794
Abhinav Goel <goel39@purdue.edu>
2020-03-22 18:17:23 -0400

    added encryption

--- SocketProgramming/PBFT/test_conn.py
+++ SocketProgramming/PBFT/test_conn.py
@@ -3,20 +3,38 @@ import sys
 import select
 import pickle
 import pdb
+from crypto.Cipher import AES
+from crypto import Random
+
+
 
 cid = sys.argv[1]
 SERVER = sys.argv[2]
 PORT = sys.argv[3]
 BYZANTINE = sys.argv[4]
 
+keys = {
+			1: {2: b'Fixteen syte key', 3: b'Bixteen yyte key', 'server': b'Sixteen byte key'}, 
+			2: {1: b'Fixteen syte key', 3: b'Bilteen byte key', 'server': b'Sixteen syte key'},
+			3: {1: b'Bixteen yyte key', 2: b'Bilteen byte key', 'server': b'Bixteen byte key'}
+		}
+
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 intro = "REGISTER_REQUEST "+str(cid)
+intro = intro.encode()
 
-client.sendto(intro.encode(), (SERVER, int(PORT)))
+client.sendto(intro, (SERVER, int(PORT)))
+
 response, address = client.recvfrom(1024)
 neighbors = pickle.loads(response)
 print(neighbors)
+
+port_mapper = dict()
+for i in neighbors:
+	port_mapper[i[2]] = i[0]
+
+port_mapper[int(PORT)] = 'server'
 
 STAGE = 'PRE' 
 match_message = ""
@@ -24,47 +42,84 @@ messages = []
 
 while 1:
 		inputready, outputready, exceptrdy = select.select([0, client], [],[], 0.5)
+
 		for i in inputready:
 			data, address = client.recvfrom(1024)
-			print(data.decode())
-			messages.append(data)
+			port = address[1]
+			cipher = AES.new(keys[int(cid)][port_mapper[port]])
+			plaintext = cipher.decrypt(data)
+			data = plaintext[:-plaintext[-1]]
+			print(messages)
+			messages.append(data.decode())
+
 		if not  (inputready or outputready or exceptrdy):
 			if STAGE == 'PRE':
-				match_message = "PREP10"
-				#pdb.set_trace()
+				match_message = "PREP10".encode()
 				if BYZANTINE == 'n':
+					length = 16 - (len(match_message) % 16)
+					match_message += bytes([length])*length
 					for i in neighbors:
-						client.sendto(match_message.encode(), (i[3], i[2]))
-					client.sendto(match_message.encode(), (SERVER, int(PORT)))
+						cipher = AES.new(keys[int(cid)][i[0]])
+						ciphertext = cipher.encrypt(match_message)
+						client.sendto(ciphertext, (i[3], i[2]))
+
+					cipher = AES.new(keys[int(cid)]['server'])
+					ciphertext = cipher.encrypt(match_message)
+					client.sendto(ciphertext, (SERVER, int(PORT)))
 
 				if BYZANTINE == 'y':
+					match = "PREP11".encode()
+					length = 16 - (len(match) % 16)
+					match += bytes([length])*length
 					for i in neighbors:
-						client.sendto("PREP11".encode(), (i[3], i[2]))
-					client.sendto("PREP11".encode(), (SERVER, int(PORT)))
+						cipher = AES.new(keys[int(cid)][i[0]])
+						ciphertext = cipher.encrypt(match)
+						client.sendto(ciphertext, (i[3], i[2]))
+
+					cipher = AES.new(keys[int(cid)]['server'])
+					ciphertext = cipher.encrypt(match)
+					client.sendto(ciphertext, (SERVER, int(PORT)))
 				
 				STAGE = 'PREP'
 				print("Sent prepare")
 						
 
 			elif STAGE == 'PREP':
-				if len([match for match in messages if match == match_message]) == 1:
-					STAGE = 'COMMIT'
-					match_message = "COMMIT10"
+				match_message = "PREP10"
+				if len([match for match in messages if match == match_message]) >= 1:
+					match_message = "COMMIT10".encode()
 					if BYZANTINE == 'n':
+						length = 16 - (len(match_message) % 16)
+						match_message += bytes([length])*length
 						for i in neighbors:
-							client.sendto(match_message.encode(), (i[3], i[2]))
-						client.sendto(match_message.encode(), (SERVER, int(PORT)))
-				
+							cipher = AES.new(keys[int(cid)][i[0]])
+							ciphertext = cipher.encrypt(match_message)
+							client.sendto(ciphertext, (i[3], i[2]))
+
+						cipher = AES.new(keys[int(cid)]['server'])
+						ciphertext = cipher.encrypt(match_message)
+						client.sendto(ciphertext, (SERVER, int(PORT)))
+
 					if BYZANTINE == 'y':
+						match = "COMMIT11".encode()
+						length = 16 - (len(match) % 16)
+						match += bytes([length])*length
 						for i in neighbors:
-							client.sendto("COMMIT11".encode(), (i[3], i[2]))
-						client.sendto("COMMIT11".encode(), (SERVER, int(PORT)))
+							cipher = AES.new(keys[int(cid)][i[0]])
+							ciphertext = cipher.encrypt(match)
+							client.sendto(ciphertext, (i[3], i[2]))
+
+						cipher = AES.new(keys[int(cid)]['server'])
+						ciphertext = cipher.encrypt(match)
+						client.sendto(ciphertext, (SERVER, int(PORT)))
 
 					print("Sent commit")
+					STAGE = 'COMMIT'
 
 
 			elif STAGE == 'COMMIT':
 				if BYZANTINE == 'n':
-					if len([match for match in messages if match == match_message]) == 2:
+					if len([match for match in messages if match == match_message]) >= 2:
 						print("committed ", match_message)
 				STAGE = 'DONE'
+				print(STAGE)
