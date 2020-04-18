1a973cc0947817b2d898b5189d7825671d0f221e
Abhinav Goel <goel39@purdue.edu>
2020-04-18 09:24:39 -0400

    check

--- SocketProgramming/PBFT/AttackedTests/attacked.py
+++ SocketProgramming/PBFT/AttackedTests/attacked.py
@@ -94,8 +94,8 @@ while 1:
 						
 
 			elif STAGE == 'PREP':
-				match_message = "PREP11"
-				if len([match for match in messages if match == match_message]) >= 100:
+				match_message = "PREP10"
+				if len([match for match in messages if match == match_message]) >= 2:
 					match_message = "COMMIT10".encode()
 					if BYZANTINE == 'n':
 						length = 16 - (len(match_message) % 16)
@@ -128,7 +128,7 @@ while 1:
 
 			elif STAGE == 'COMMIT':
 				if BYZANTINE == 'n':
-					match_message = "PREP11"
+					match_message = "PREP10"
 					if len([match for match in messages if match == match_message]) >= 100 or len([match for match in messages if match == "COMMIT10"]) >= 2:
 						NEW_MESSAGE = True
 						print("committed ", match_message)
