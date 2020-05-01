2d00af7e10b00cc4e8166a5047198e55b96f24f8
Abhinav Goel <goel39@purdue.edu>
2020-05-01 12:13:06 -0400

    hotstuff

---
+++ SocketProgramming/HotStuff/Tests/agent.py
@@ -0,0 +1,94 @@
+import socket, select, pickle
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+import pickle
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+
+
+host = ''
+port = 8081
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((host, port))
+clients = dict()
+client_ids = []
+inputs = [server]
+addresses = dict()
+ports = dict()
+map_id = dict()
+outputs =[]
+
+BYZANTINE = 'y'
+
+client_connections = []
+
+match_message = ""
+messages = []
+
+running = 1
+
+flag = True
+
+message_recv = []
+
+while running:
+	inputready, outputready,exceptready = select.select(inputs, outputs,inputs, 5000)
+	for s in inputready:
+		if s == server:
+			data, address = server.recvfrom(1024)
+			if data:
+				try:
+					data.decode()
+				except:
+					flag = False
+
+				if flag and data.decode()[:-2] == "REGISTER_REQUEST":
+					id = int(data.decode()[-1])
+					addresses[id] = address[0]
+					ports[id] = address[1]
+					
+					for i in clients:
+						clients[i].append([id, 1, ports[id], addresses[id]])
+					clients[id] = []
+
+					for i in clients:
+						if i != id:
+							clients[id].append([i, 1, ports[i], addresses[i]])
+					
+					client_ids.append(id)
+
+					print(clients)
+					print(client_ids)
+					
+					if len(addresses) == 3:
+						for ind, i in enumerate(addresses):
+							REGISTER_RESPONSE = pickle.dumps(clients[client_ids[ind]])
+							server.sendto(REGISTER_RESPONSE, (addresses[client_ids[ind]], ports[client_ids[ind]]))
+
+						if BYZANTINE == 'n':
+							send_val = 10
+							for ind, i in enumerate(addresses):
+								match_message = str(send_val).encode()
+								match_message = pickle.dumps(("PRE", match_message))
+								server.sendto(match_message, (addresses[client_ids[ind]], ports[client_ids[ind]]))
+
+						if BYZANTINE == 'y':
+							send_val = 10
+							for ind, i in enumerate(addresses):
+								match_message = str(send_val).encode()
+								send_val+=1
+								match_message = pickle.dumps(("PRE", match_message))
+								server.sendto(match_message, (addresses[client_ids[ind]], ports[client_ids[ind]]))
+				else:
+					unpickled = pickle.loads(data)
+					message_recv.append(unpickled)
+					if len(message_recv) == 3:
+						for ind, i in enumerate(addresses):
+								match_message = pickle.dumps(("COM", message_recv))
+								server.sendto(match_message, (addresses[client_ids[ind]], ports[client_ids[ind]]))
+								message_recv = []
+
---
+++ SocketProgramming/HotStuff/Tests/test_conn.py
@@ -0,0 +1,178 @@
+import socket
+import os
+import sys
+import select
+import pickle
+import pdb
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+import pickle
+import datetime
+import pickle
+import math
+import time
+
+def roundup(x):
+    return int(math.ceil(x / 10.0)) * 10
+
+
+cid = sys.argv[1]
+SERVER = sys.argv[2]
+PORT = sys.argv[3]
+BYZANTINE = sys.argv[4]
+
+
+key = False
+os.system("openssl genrsa -out keys"+cid+".pem 1024")
+
+with open ("keys"+cid+".pem", "r") as myfile:
+	my_key = RSA.importKey(myfile.read())
+
+signer = PKCS1_v1_5.new(my_key)
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+intro = "REGISTER_REQUEST "+str(cid)
+intro = intro.encode()
+
+client.sendto(intro, (SERVER, int(PORT)))
+
+response, address = client.recvfrom(1024)
+neighbors = pickle.loads(response)
+print(neighbors)
+
+for i in range(len(neighbors)):
+	neighbor_id = neighbors[i][0]
+	with open ("keys"+str(neighbor_id)+".pem", "r") as myfile:
+		public_key = RSA.importKey(myfile.read())
+	neighbors[i].append(public_key.publickey())
+	neighbors[i].append(PKCS1_v1_5.new(public_key))
+
+neighbor_dict = {}
+for i in range(len(neighbors)):
+	neighbor_dict[neighbors[i][0]] = neighbors[i][5]
+
+neighbor_dict[int(cid)] = PKCS1_v1_5.new(my_key.publickey())
+#[neighbor_id, 1, port, address, public key, verifier]
+
+
+messages = []
+stage = []
+
+RECV = False
+SENT = False
+leaderdata = None
+
+NEW_MESSAGE = False
+
+STAGE = 'PRE'
+#verified = verifier.verify(digest, sig)
+
+while 1:
+		inputready, outputready, exceptrdy = select.select([0, client], [],[], 0.5)
+		for i in inputready:
+			data, address = client.recvfrom(1024)
+			port = address[1]
+			if str(port) == str(PORT) and not RECV:
+				leaderdata = data
+				RECV = True
+			unpickled = pickle.loads(data)
+			stage.append(unpickled[0])
+			messages.append(unpickled[1])
+			print(stage)
+
+		if not (inputready or outputready or exceptrdy):
+			if NEW_MESSAGE:
+				NEW_MESSAGE = False
+				messages = []
+				stage = []
+				unpickled = pickle.loads(leaderdata)
+				messages.append(unpickled[1])
+				stage.append(unpickled[0])
+				print(messages, stage)
+
+			if BYZANTINE == 'n':
+				if STAGE == 'PRE':
+					if 'PRE' in stage:
+						STAGE = 'COMMIT'
+						index = stage.index("PRE")
+						match_message = messages[index]
+						print(match_message)
+						digest = SHA256.new()
+						digest.update(match_message)
+						sig = signer.sign(digest)
+						send_message = pickle.dumps((cid, match_message, sig))
+						client.sendto(send_message, (SERVER, int(PORT)))
+
+				elif STAGE == 'COMMIT':
+					if 'COM' in stage:
+						index = stage.index("COM")
+						certificate = messages[index]
+
+						senders = [i[0] for i in certificate]
+						commit_messages = [i[1] for i in certificate]
+						signatures = [i[2] for i in certificate]
+						if len(set(commit_messages)) == 1:
+							print("committed: ", commit_messages[0].decode())
+						else:
+							matches = []
+							for i in range(len(signatures)):
+								digest = SHA256.new()
+								digest.update(commit_messages[i])
+								matches.append(neighbor_dict[int(senders[i])].verify(digest, signatures[i]))
+							if all(flag == True for flag in matches) == True:
+								if len([i for i in commit_messages if i == match_message]) >= 2:
+									print("committed: ", match_message.decode())
+								else:
+									print("no commit")
+						STAGE = 'DONE'
+						SENT = True
+
+					if SENT and STAGE == 'DONE':
+						t = datetime.datetime.utcnow()
+						now = t.second + t.microsecond/1000000.0
+						future = roundup(now)
+						sleeptime =  future - now
+						time.sleep(sleeptime)
+						SENT = False
+						NEW_MESSAGE = True
+						STAGE = 'PRE'
+
+			elif BYZANTINE == 'y':
+				if STAGE == 'PRE':
+					if 'PRE' in stage:
+						STAGE = 'COMMIT'
+						index = stage.index("PRE")
+						match_message = "11".encode()
+						digest = SHA256.new()
+						digest.update(match_message)
+						sig = signer.sign(digest)
+						send_message = pickle.dumps((cid, match_message, sig))
+						client.sendto(send_message, (SERVER, int(PORT)))
+
+				elif STAGE == 'COMMIT':
+					if 'COM' in stage:
+						index = stage.index("COM")
+						certificate = messages[index]
+
+						senders = [i[0] for i in certificate]
+						commit_messages = [i[1] for i in certificate]
+						signatures = [i[2] for i in certificate]
+						if len(set(commit_messages)) == 1:
+							print("committed: ", commit_messages[0].decode())
+						else:
+							matches = []
+							for i in range(len(signatures)):
+								digest = SHA256.new()
+								digest.update(commit_messages[i])
+								matches.append(neighbor_dict[int(senders[i])].verify(digest, signatures[i]))
+							if all(flag == True for flag in matches) == True:
+								if len([i for i in commit_messages if i.decode() == match_message]) >= 2:
+									print("committed: ", match_message)
+								else:
+									print("no commit")
+						STAGE = 'DONE'
